2e39d111f169f842bd2ee09e0759c22bcf90bf02
Pokeylooted <pokeyrandomgaming@gmail.com>
2023-02-19 00:41:13 -0600

    Added more styles, and password show/hide
    [BUG] Password show/hide, not working properly

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ShowHidePasswordDirective } from '../show-hide-password.directive';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  @ViewChild(ShowHidePasswordDirective)
+  private showHidePasswordDirective!: ShowHidePasswordDirective;
+  @ViewChild('passwordInput')
+  private passwordInput!: ElementRef;
 
+  ngAfterViewInit() {
+    const passwordInputEl = this.passwordInput.nativeElement;
+    passwordInputEl.addEventListener('blur', () => {
+      const iconEl = this.showHidePasswordDirective.el.nativeElement.querySelector('i');
+      passwordInputEl.type = 'password';
+      iconEl.classList.remove(this.showHidePasswordDirective.eyeSlashIconClass);
+      iconEl.classList.add(this.showHidePasswordDirective.eyeIconClass);
+    });
+  }
 }
