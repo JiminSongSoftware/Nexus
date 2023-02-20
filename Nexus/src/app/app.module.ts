91c30b17d4febe6f20eb6ee1e43f4e03fbe37c87
Ji Min Song <sjm9509@gmail.com>
2023-02-20 02:22:13 -0800

    rebase

---
+++ Nexus/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { environment } from 'src/environment';
+import { ShowHidePasswordDirective } from './show-hide-password.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    ShowHidePasswordDirective,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [{ provide: 'API_URL', useValue: environment.apiUrl }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Nexus/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.sass'],
+})
+
+
+
+export class DashboardComponent {
+
+  private _isLoggedIn = true
+  constructor(private authService: AuthService) {
+    this._isLoggedIn = this.authService.isLoggedIn();
+  }
+  
+  public get isLoggedIn(): boolean {
+    return this._isLoggedIn;
+  }
+
+}
